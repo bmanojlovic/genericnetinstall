518f9bcc7fc3c8ab7b927c47285c075a83e32358
Boris Manojlovic <boris.manojlovic@zis.co.rs>
2012-07-27 17:19:43 +0200

    fixed typo

--- web-dir/boot.php
+++ web-dir/boot.php
@@ -19,8 +19,8 @@ menu iPXE boot menu for Zajednicki Informacioni Sistem
 
 item --gap --           ------------------- OS images ----------------
 item --key s suse       SUSE based distributions (openSUSE and SLE)
-item --key r redhat     RedHat based distribtions (RedHat and Centos)
-item --key d debian     Debian based distribtions (Debian and Ubuntu)
+item --key r redhat     RedHat based distributions (RedHat and Centos)
+item --key d debian     Debian based distributions (Debian and Ubuntu)
 item --gap --           ----------- Old based PXE linux menu ---------
 item --key p pxelinux   PXElinux menu based system
 item --gap --           --------------- Advanced Menu ----------------
